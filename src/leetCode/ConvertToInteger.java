1bf1ab3b078f803057894e052d259f058f87c120
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-06 23:23:46 +0100

    program

--- src/leetCode/ConvertToInteger.java
+++ src/leetCode/ConvertToInteger.java
@@ -1,7 +1,12 @@
 package leetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConvertToInteger {
     public static void main(String[] args) {
-
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
     }
 }
