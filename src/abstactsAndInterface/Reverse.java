7ec882481148089f5715f0a77bc2ab5ef90aedde
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-08 15:51:22 +0100

    myString

---
+++ src/abstactsAndInterface/Reverse.java
@@ -0,0 +1,26 @@
+package abstactsAndInterface;
+
+import java.util.Scanner;
+
+public class Reverse {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        reverse();
+    }
+
+    public static void collectInput(){
+        System.out.println("Enter the word(s) to be reversed: ");
+    }
+    public static  void reverse(){
+        collectInput();
+        String in = scanner.nextLine();
+
+        for (int i = in.length() - 1; i >= 0 ; i--) {
+            System.out.print(in.charAt(i));
+        }
+    }
+
+
+
+
+}
