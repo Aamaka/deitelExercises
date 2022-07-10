2568f9305fd4641ba0f0958941f806ca917f3180
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-10 21:26:48 +0100

    work

--- src/personalFun/Generate.java
+++ src/personalFun/Generate.java
@@ -1,7 +1,18 @@
 package personalFun;
 
+import java.security.SecureRandom;
+
 public class Generate {
     public static void main(String[] args) {
+        String a = "0123456789";
 
+        char[] arr = new  char[10];
+        SecureRandom random = new SecureRandom();
+
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = a.charAt(random.nextInt(a.length()));
+        }
+        System.out.println("your account number is: "+ new String(arr));
     }
 }
