6e0623527b09696d3ec13ba55a4d2a55c6c74a0f
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-22 12:19:22 +0100

    work

--- src/classActivities/File.java
+++ src/classActivities/File.java
@@ -1,18 +1,27 @@
 package classActivities;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class File {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         Path path = Paths.get(scanner.nextLine());
 
         if(Files.exists(path)){
+            System.out.printf("%n%s exists ", path.getFileName());
+            System.out.printf("%n%s a directory ", Files.isDirectory(path)? "is" : "is Not");
+            System.out.printf("%n%s absolute ", path.isAbsolute() ? "is": "is not");
+            System.out.printf("%n%s last modified time  ", Files.getLastModifiedTime(path));
+            System.out.printf("%n size : %s", Files.size(path));
+            System.out.printf("%n Absolute %s", path.toAbsolutePath());
 
+        }else {
+            System.out.println("path does not exist");
         }
     }
 }
