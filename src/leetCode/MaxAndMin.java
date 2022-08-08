ab9679a22928f0f3e8920e75ee594bc3603b062f
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-08-08 22:08:51 +0100

    work

--- src/leetCode/MaxAndMin.java
+++ src/leetCode/MaxAndMin.java
@@ -1,20 +1,31 @@
 package leetCode;
 
+import java.util.Arrays;
+
+import static java.util.stream.IntStream.of;
+
 public class MaxAndMin {
     public static void main(String[] args) {
         int[] arr = {2, 16, 4, 77};
         System.out.println(max(arr));
         System.out.println(min(arr));
     }
-    public static int max(int[] arr){
+    public static int maximum(int[] arr){
         int maxi = arr[0];
         for (int m : arr) {
             if (m > maxi) maxi = m;
         }
         return maxi;
     }
+    public static int max(int[] list) {
+        return of(list).max().getAsInt();
+}
 
-    public static  int min(int[] arr){
+    public static int min(int[] list) {
+        return Arrays.stream(list).min().getAsInt();
+    }
+
+    public static  int minimum(int[] arr){
         int maxi = arr[0];
         for (int m : arr) {
             if (m < maxi) maxi = m;
